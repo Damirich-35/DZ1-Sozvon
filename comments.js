a9d53bfa9c1c624a4d77b847f073c23a63da9f91
Damirich-35 <www.dmr777@mail.ru>
2026-08-13 01:04:28 +0500

    feat: complete homework 9 level 1 tasks, add array filtering, inclusions and reversal function

---
+++ comments.js
@@ -0,0 +1,72 @@
+export const socialMediaComments = [
+{
+postId: 1,
+id: 1,
+name: "id labore ex et quam laborum",
+email: "Eliseo@gardner.biz",
+body: "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"
+},
+{
+postId: 1,
+id: 2,
+name: "quo vero reiciendis velit similique earum",
+email: "Jayne_Kuhic@sydney.com",
+body: "est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et"
+},
+{
+postId: 1,
+id: 3,
+name: "odio adipisci rerum aut animi",
+email: "Nikita@garfield.biz",
+body: "quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam\nest et quasi labore et voluptatem"
+},
+{
+postId: 1,
+id: 4,
+name: "alias odio sit",
+email: "Lew@alysha.tv",
+body: "non et expedita accusantium unde eius\neaque aut occaecati qui quia quis expedita\nvoluptatem quia voluptas dolor\nnoque voluptatem qui deleniti commodi"
+},
+{
+postId: 1,
+id: 5,
+name: "vero eaque aliquid doloribus et culpa",
+email: "Hayden@althea.biz",
+body: "harum non quasi et ratione\ntempore iure ex voluptates in ratione\nharum architecto fugit inventore cupiditate\nvoluptates magni quo et"
+},
+{
+postId: 2,
+id: 6,
+name: "et fugit eligendi deleniti quidem qui sint nihil autem",
+email: "Presley.Mueller@myrl.com",
+body: "doloribus at sed quis culpa deserunt id eos ea\nconsequatur et nam eum in ipsa quia\nconsequatur ut id sed dolores\nsit iusto eligendi eos expedita"
+},
+{
+postId: 2,
+id: 7,
+name: "repellat consequatur praesentium vel ut eleifend",
+email: "Dallas@ole.me",
+body: "maiores sed dolores similique labore et inventore laudantium\nvoluptate sit omnis omnis id dolor\nratione quia et eos error ea varius\net expedita quasi aut"
+},
+{
+postId: 2,
+id: 8,
+name: "et omnis dolorem",
+email: "Mallory_Fritsch@coder.net",
+body: "ut voluptatem corrupti velit\nad voluptatem maiores et\nomnis velit porro iusto accusamus\nfugit ut id laudantium enim quasi est"
+},
+{
+postId: 2,
+id: 9,
+name: "provident id voluptas",
+email: "Meghan_Littel@rene.us",
+body: "sapiente assumenda molestiae atque\nadipisci laborum distinctio aperiam et transient\nquia eos rerum aut quia sunt\nut autem est facilis expedita"
+},
+{
+postId: 2,
+id: 10,
+name: "eaque et iusto incidunt est et",
+email: "Carmen_Keeling@caroline.com",
+body: "voluptate iusto quis nobis reprehenderit ipsum amet nulla\nquia quas dolores velit et voluptatem\nprovident consequuntur voluptatum laboriosam\nomnis inventore architecto omnis sint unde"
+}
+];
--- homework-9.js
+++ homework-9.js
@@ -1,12 +1,12 @@
 
-// TASK 2: Filter array of numbers starting from 5
+// TASK 2:
 const initialNumbersSequence =[1,2,3,4,5,6,7,8,9,10];
 const filteredNumbersSequence = initialNumbersSequence.filter(function(number) {
     return number >= 5;
 });
 
 
-// TASK 3: Check if a specific string exists in the array
+// TASK 3:
 const kitchenAppliancesList = [
   'Toaster',
   'mixer',
@@ -17,10 +17,48 @@ const kitchenAppliancesList = [
 
 const hasSpecificAppliance = kitchenAppliancesList.includes('Toaster');
 
-// TASK 4: Function to reverse any array and reversing both arrays from tasks 2 and 3
+// TASK 4:
 function getReversedArray(targetArray) {
   return [...targetArray].reverse();
 }
 
 const reversedNumbersResult = getReversedArray(filteredNumbersSequence);
 const reversedAppliancesResult = getReversedArray(kitchenAppliancesList);
+// TASK 6:
+import { socialMediaComments } from './comments.js'; 
+
+// TASK 7:
+const dotComComments = socialMediaComments.filter(comment => comment.email.includes('.com'));
+console.log('TASK 7 (.com comments):', dotComComments); 
+
+// TASK 8:
+const updatedPostIdComments = socialMediaComments.map(comment => {
+return {
+...comment,
+postId: comment.id <= 5 ? 2 : 1
+};
+});
+console.log('TASK 8 (updated postId):', updatedPostIdComments); 
+
+// TASK 9:
+const simplifiedComments = socialMediaComments.map(comment => {
+return {
+id: comment.id,
+name: comment.name
+};
+});
+console.log('TASK 9 (only id and name):', simplifiedComments); 
+
+// TASK 10:
+const validatedComments = socialMediaComments.map(comment => {
+return {
+...comment,
+isInvalid: comment.body.length > 180
+};
+});
+console.log('TASK 10 (validated comments):', validatedComments);
+console.log('TASK 11 (Emails via reduce):', emailsWithReduce);
+const emailsWithMap = socialMediaComments.map(function(comment) {return comment.email;});
+console.log('TASK 11 (Emails via map):', emailsWithMap)
+const emailsStringJoin = emailsWithReduce.join(', ');console.log('TASK 12 (via join):', emailsStringJoin);
+const emailsStringToString = emailsWithReduce.toString();console.log('TASK 12 (via toString):', emailsStringToString);
