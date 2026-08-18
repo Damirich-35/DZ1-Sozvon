aa173216cb02e6f90e256a340921e629202514f6
Damirich-35 <www.dmr777@mail.ru>
2026-08-19 01:15:01 +0500

    fix: форматирование длинного текста в comments.js

--- comments.js
+++ comments.js
@@ -3,29 +3,41 @@ export const socialMediaComments = [
 postId: 1,
 id: 1,
 name: "id labore ex et quam laborum",
-email: "Eliseo@gardner.biz",
-body: "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"
+email: "Eliseo@gardner.biz",body: `laudantium enim quasi est quidem magnam voluptate ipsam eos
+tempora quo necessitatibus
+dolor quam autem quasi
+reiciendis et nam sapiente accusantium`
+
 },
 {
 postId: 1,
 id: 2,
 name: "quo vero reiciendis velit similique earum",
 email: "Jayne_Kuhic@sydney.com",
-body: "est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et"
+body:`est natus enim nihil est dolore omnis voluptatem numquam
+et omnis occaecati quod ullam at
+voluptatem error expedita pariatur
+nihil sint nostrum voluptatem reiciendis et`
 },
 {
 postId: 1,
 id: 3,
 name: "odio adipisci rerum aut animi",
 email: "Nikita@garfield.biz",
-body: "quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam\nest et quasi labore et voluptatem"
+body: `quia molestiae reprehenderit quasi aspernatur
+aut expedita occaecati aliquam eveniet laudantium
+omnis quibusdam delectus saepe quia accusamus maiores nam
+est et quasi labore et voluptatem`
 },
 {
 postId: 1,
 id: 4,
 name: "alias odio sit",
 email: "Lew@alysha.tv",
-body: "non et expedita accusantium unde eius\neaque aut occaecati qui quia quis expedita\nvoluptatem quia voluptas dolor\nnoque voluptatem qui deleniti commodi"
+body: `non et expedita accusantium unde eius
+eaque aut occaecati qui quia quis expedita
+voluptatem quia voluptas dolor
+noque voluptatem qui deleniti commodi`
 },
 {
 postId: 1,
@@ -39,7 +51,8 @@ postId: 2,
 id: 6,
 name: "et fugit eligendi deleniti quidem qui sint nihil autem",
 email: "Presley.Mueller@myrl.com",
-body: "doloribus at sed quis culpa deserunt id eos ea\nconsequatur et nam eum in ipsa quia\nconsequatur ut id sed dolores\nsit iusto eligendi eos expedita"
+body: `doloribus at sed quis culpa deserunt id eos ea
+consequatur et nam eum in ipsa quia\nconsequatur ut id sed dolores\nsit iusto eligendi eos expedit`
 },
 {
 postId: 2,
@@ -53,20 +66,28 @@ postId: 2,
 id: 8,
 name: "et omnis dolorem",
 email: "Mallory_Fritsch@coder.net",
-body: "ut voluptatem corrupti velit\nad voluptatem maiores et\nomnis velit porro iusto accusamus\nfugit ut id laudantium enim quasi est"
+body: `ut voluptatem corrupti velit
+ad voluptatem maiores et
+omnis velit porro iusto accusamus
+fugit ut id laudantium enim quasi est`
 },
 {
 postId: 2,
 id: 9,
 name: "provident id voluptas",
 email: "Meghan_Littel@rene.us",
-body: "sapiente assumenda molestiae atque\nadipisci laborum distinctio aperiam et transient\nquia eos rerum aut quia sunt\nut autem est facilis expedita"
+body: `sapiente assumenda molestiae atque
+adipisci laborum distinctio aperiam et transient
+quia eos rerum aut quia sunt\nut autem est facilis expedita`
 },
 {
 postId: 2,
 id: 10,
 name: "eaque et iusto incidunt est et",
 email: "Carmen_Keeling@caroline.com",
-body: "voluptate iusto quis nobis reprehenderit ipsum amet nulla\nquia quas dolores velit et voluptatem\nprovident consequuntur voluptatum laboriosam\nomnis inventore architecto omnis sint unde"
+body: `voluptate iusto quis nobis reprehenderit ipsum amet nulla
+quia quas dolores velit et voluptatem
+provident consequuntur voluptatum laboriosam
+omnis inventore architecto omnis sint unde`
 }
 ];
