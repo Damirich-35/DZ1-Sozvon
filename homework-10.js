47400cb35a8ad54d04a4aafa1bc15337def6bcd4
Damirich-35 <www.dmr777@mail.ru>
2026-08-30 23:32:16 +0500

    fix: принудительно выровнял код под 2 пробела

--- homework-10.js
+++ homework-10.js
@@ -1,95 +1,95 @@
 export const socialMediaComments = [
-{
-postId: 1,
-id: 1,
-name: "id labore ex et quam laborum",
-email: "Eliseo@gardner.biz",body: `laudantium enim quasi est quidem magnam voluptate ipsam eos
+  {
+    postId: 1,
+    id: 1,
+    name: "id labore ex et quam laborum",
+    email: "Eliseo@gardner.biz", body: `laudantium enim quasi est quidem magnam voluptate ipsam eos
 tempora quo necessitatibus
 dolor quam autem quasi
 reiciendis et nam sapiente accusantium`
 
-},
-{
-postId: 1,
-id: 2,
-name: "quo vero reiciendis velit similique earum",
-email: "Jayne_Kuhic@sydney.com",
-body:`est natus enim nihil est dolore omnis voluptatem numquam
+  },
+  {
+    postId: 1,
+    id: 2,
+    name: "quo vero reiciendis velit similique earum",
+    email: "Jayne_Kuhic@sydney.com",
+    body: `est natus enim nihil est dolore omnis voluptatem numquam
 et omnis occaecati quod ullam at
 voluptatem error expedita pariatur
 nihil sint nostrum voluptatem reiciendis et`
-},
-{
-postId: 1,
-id: 3,
-name: "odio adipisci rerum aut animi",
-email: "Nikita@garfield.biz",
-body: `quia molestiae reprehenderit quasi aspernatur
+  },
+  {
+    postId: 1,
+    id: 3,
+    name: "odio adipisci rerum aut animi",
+    email: "Nikita@garfield.biz",
+    body: `quia molestiae reprehenderit quasi aspernatur
 aut expedita occaecati aliquam eveniet laudantium
 omnis quibusdam delectus saepe quia accusamus maiores nam
 est et quasi labore et voluptatem`
-},
-{
-postId: 1,
-id: 4,
-name: "alias odio sit",
-email: "Lew@alysha.tv",
-body: `non et expedita accusantium unde eius
+  },
+  {
+    postId: 1,
+    id: 4,
+    name: "alias odio sit",
+    email: "Lew@alysha.tv",
+    body: `non et expedita accusantium unde eius
 eaque aut occaecati qui quia quis expedita
 voluptatem quia voluptas dolor
 noque voluptatem qui deleniti commodi`
-},
-{
-postId: 1,
-id: 5,
-name: "vero eaque aliquid doloribus et culpa",
-email: "Hayden@althea.biz",
-body: "harum non quasi et ratione\ntempore iure ex voluptates in ratione\nharum architecto fugit inventore cupiditate\nvoluptates magni quo et"
-},
-{
-postId: 2,
-id: 6,
-name: "et fugit eligendi deleniti quidem qui sint nihil autem",
-email: "Presley.Mueller@myrl.com",
-body: `doloribus at sed quis culpa deserunt id eos ea
+  },
+  {
+    postId: 1,
+    id: 5,
+    name: "vero eaque aliquid doloribus et culpa",
+    email: "Hayden@althea.biz",
+    body: "harum non quasi et ratione\ntempore iure ex voluptates in ratione\nharum architecto fugit inventore cupiditate\nvoluptates magni quo et"
+  },
+  {
+    postId: 2,
+    id: 6,
+    name: "et fugit eligendi deleniti quidem qui sint nihil autem",
+    email: "Presley.Mueller@myrl.com",
+    body: `doloribus at sed quis culpa deserunt id eos ea
 consequatur et nam eum in ipsa quia\nconsequatur ut id sed dolores\nsit iusto eligendi eos expedit`
-},
-{
-postId: 2,
-id: 7,
-name: "repellat consequatur praesentium vel ut eleifend",
-email: "Dallas@ole.me",
-body: "maiores sed dolores similique labore et inventore laudantium\nvoluptate sit omnis omnis id dolor\nratione quia et eos error ea varius\net expedita quasi aut"
-},
-{
-postId: 2,
-id: 8,
-name: "et omnis dolorem",
-email: "Mallory_Fritsch@coder.net",
-body: `ut voluptatem corrupti velit
+  },
+  {
+    postId: 2,
+    id: 7,
+    name: "repellat consequatur praesentium vel ut eleifend",
+    email: "Dallas@ole.me",
+    body: "maiores sed dolores similique labore et inventore laudantium\nvoluptate sit omnis omnis id dolor\nratione quia et eos error ea varius\net expedita quasi aut"
+  },
+  {
+    postId: 2,
+    id: 8,
+    name: "et omnis dolorem",
+    email: "Mallory_Fritsch@coder.net",
+    body: `ut voluptatem corrupti velit
 ad voluptatem maiores et
 omnis velit porro iusto accusamus
 fugit ut id laudantium enim quasi est`
-},
-{
-postId: 2,
-id: 9,
-name: "provident id voluptas",
-email: "Meghan_Littel@rene.us",
-body: `sapiente assumenda molestiae atque
+  },
+  {
+    postId: 2,
+    id: 9,
+    name: "provident id voluptas",
+    email: "Meghan_Littel@rene.us",
+    body: `sapiente assumenda molestiae atque
 adipisci laborum distinctio aperiam et transient
 quia eos rerum aut quia sunt\nut autem est facilis expedita`
-},
-{
-postId: 2,
-id: 10,
-name: "eaque et iusto incidunt est et",
-email: "Carmen_Keeling@caroline.com",
-body: `voluptate iusto quis nobis reprehenderit ipsum amet nulla
+  },
+  {
+    postId: 2,
+    id: 10,
+    name: "eaque et iusto incidunt est et",
+    email: "Carmen_Keeling@caroline.com",
+    body: `voluptate iusto quis nobis reprehenderit ipsum amet nulla
 quia quas dolores velit et voluptatem
 provident consequuntur voluptatum laboriosam
 omnis inventore architecto omnis sint unde`
-}
+  }
 ];
 import { productsList } from './products.js';
 console.log('Задание 2 (Список товаров):', productsList);
@@ -112,14 +112,14 @@ function createProductCardTemplate(product) {
     </div>
   `;
   // ЗАДАНИЕ 4: Группировка продуктов через .reduce()
-const productsMap = productsList.reduce((accumulator, product) => {
-  accumulator[product.title] = {
-    price: product.price,
-    ingredients: product.ingredients
-  };
-  return accumulator;
-}, {});
-console.log('Задание 4 (Объект продуктов):', productsMap);
+  const productsMap = productsList.reduce((accumulator, product) => {
+    accumulator[product.title] = {
+      price: product.price,
+      ingredients: product.ingredients
+    };
+    return accumulator;
+  }, {});
+  console.log('Задание 4 (Объект продуктов):', productsMap);
 }
 // ФУНКЦИЯ 1: Только запрашивает число у пользователя, проверяет его и возвращает
 function getCountFromUser() {
@@ -138,7 +138,7 @@ function getCountFromUser() {
 // ФУНКЦИЯ 2: Только рендерит карточки на страницу, принимая массив аргументом
 function renderProductCards(productsArray) {
   const container = document.querySelector('.products-container');
-  
+
   if (!container) {
     console.error("Контейнер .products-container не найден!");
     return;
@@ -162,7 +162,7 @@ const numberOfCards = getCountFromUser();
 if (numberOfCards !== null) {
   // Вырезаем из нашего главного списка ровно столько товаров, сколько просил пользователь
   const productsToRender = productsList.slice(0, numberOfCards);
-  
+
   // Передаем этот кусочек массива аргументом в функцию рендеринга
   renderProductCards(productsToRender);
 }
