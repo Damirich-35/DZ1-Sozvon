87c3aedfbdf5903ed0718b350a8f4e8c3c2b2737
Damirich-35 <www.dmr777@mail.ru>
2026-09-10 22:48:20 +0500

    fix: поправил картинки и табуляцию

--- products.js
+++ products.js
@@ -4,34 +4,34 @@ export const productsList = [
     title: "Гель для умывания",
     price: 1650,
     ingredients: ["минералы", "витамины С, А, PP, B и E", "солнцезащитные компоненты"],
-    image: "img/fase-wash-gel.jpg"
+    image: "fase-wash-gel"
   },
   {
     id: 2,
     title: "Увлажняющая маска",
     price: 3500,
     ingredients: ["воски", "минералы", "масла"],
-    image: "img/mask.jpg"
+    image: "mask"
   },
   {
     id: 3,
     title: "Подарочный набор №1",
     price: 4750,
     ingredients: ["воски", "минералы", "масла"],
-    image: "img/set-1.jpg"
+    image: "set-1"
   }, 
   {
     id: 4,
     title: "Подарочный набор №5",
     price: 7520,
     ingredients: ["воски", "минералы", "масла"],
-    image: "img/set-5.jpg"
+    image: "set-5"
   },
   {
     id: 5,
     title: "Тоник для лица",
     price: 2100,
     ingredients: ["минералы", "экстракты трав"],
-    image: "img/mask.jpg"
+    image: "mask"
   }
 ];
